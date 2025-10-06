aa07948800a09a0fd135eaceb5e261b02c1efb68
alilo113 <65548875+alilo113@users.noreply.github.com>
2025-10-06 17:00:26 +0100

    Update email.py

--- email.py
+++ email.py
@@ -1,11 +1,27 @@
-from imaplib import IMAP4
+from imaplib import IMAP4, IMAP4_SSL
 
-def resive_emails_from_mailbox(mail: IMAP4, mailbox: str = "INBOX") -> list:
+def email_ingestion(
+    host: str,
+    username: str,
+    password: str,
+    mailbox: str = "INBOX",
+    port: int = 993,
+    use_ssl: bool = True,
+    search_criteria: str = 'ALL',
+):
+    if use_ssl:
+        mail = IMAP4_SSL(host, port)
+    else:
+        mail = IMAP4(host, port)
+    
+    mail.login(username, password)
     mail.select(mailbox)
-    result, data = mail.search(None, "ALL")
+    result, data = mail.search(None, search_criteria)
     email_ids = data[0].split()
+    
     emails = []
+
     for email_id in email_ids:
-        result, msg_data = mail.fetch(email_id, "(RFC822)")
-        emails.append(msg_data[0][1])
-    return emails
+        result, msg_data = mail.fetch(email_id, '(RFC822)')
+        if result == 'OK':
+            emails.append(msg_data[0][1])
