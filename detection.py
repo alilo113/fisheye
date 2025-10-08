54979e31cdc46fa6b80ae03b074a510b3dc52c86
alilo113 <65548875+alilo113@users.noreply.github.com>
2025-10-08 10:43:07 +0100

    Update detection.py

--- detection.py
+++ detection.py
@@ -1,1 +1,12 @@
-print("hello wolrd")
+from email import message_from_bytes
+import imaplib
+import dns.resolver
+import spf
+import dkim
+import tldextract
+import preprocessing
+
+def reading_sender_info(email_message):
+    m = message_from_bytes(email_message)
+    sender = m["From"]
+    return f'This message is from {sender}'
